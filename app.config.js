e81b8f57e6dd23a1c49beca86780f04986719c19
lucasdwn <lfcosta0804@gmail.com>
2024-11-08 00:07:58 -0300

    🚀 Configuraçao build apk
    Configuração para buildar apk

--- app.config.js
+++ app.config.js
@@ -1,42 +1,46 @@
-export default {
-  expo: {
-    name: "DaVinciCare",
-    slug: "davinci-care",
-    version: "1.0.0",
-    orientation: "portrait",
-    icon: "./assets/logo.png",
-    userInterfaceStyle: "light",
-    
-    splash: {
-      image: "./assets/splash.png",
-      resizeMode: "contain",
-      backgroundColor: "#ffffff",
-    },
-    
-    ios: {
-      supportsTablet: true,
-    },
-    
-    android: {
-      package: "com.daVinciCodes.daVinciCare",
-      adaptiveIcon: {
-        foregroundImage: "./assets/adaptive-icon.png",
-        backgroundColor: "#ffffff",
-      },
-    },
-    
-    web: {
-      favicon: "./assets/favicon.png",
-    },
-
-    plugins: [
-      "expo-font",
-    ],
-
-    extra: {
-      eas: {
-        projectId: "0a598b76-6e6a-498e-94dd-7514ff24e5f8", // Adicione o seu projectId aqui
-      },
-    },
+// app.config.js
+export default ({ config }) => ({
+  ...config,
+  name: "Da vinci Care",
+  slug: "da-vinci-care",
+  version: "1.0.0",
+  "owner": "lucasdwn",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light", // ou 'automatic' para alternância automática
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain", // ou 'cover'
+    backgroundColor: "#ffffff"
   },
-};
+  android: {
+    package: "com.davincicodes.davincicare", // Ex: com.exemplo.nomeapp
+    versionCode: 1,
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#FFFFFF"
+    },
+    permissions: [], // adicione permissões aqui se necessário
+  },
+  ios: {
+    bundleIdentifier: "com.davincicodes.davincicare",
+    supportsTablet: true
+  },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: ["**/*"],
+  plugins: [
+    [
+      "expo-image-picker",
+      {
+        photosPermission: "O aplicativo precisa de permissão para acessar suas fotos."
+      }
+    ]
+  ],
+  extra: {
+    "eas": {
+      "projectId": "3ca638f1-162a-4a04-85ba-31178b6ffb1e"
+    }
+  }
+});
